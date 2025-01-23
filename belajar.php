6058e21ea0238c80ff8c8e2a8abec2a68c8c1918
Valz08 <revalvarl08@gmail.com>
2025-01-23 08:49:53 +0700

    first commit

---
+++ belajar.php
@@ -0,0 +1,24 @@
+<?php 
+
+// $i = 1;
+// while($i < 5 ){
+//     echo "taekkkkk ";
+// $i++;    
+// }
+
+// $i = 10;
+// do {
+//     echo "hayu mabaar ";
+// $i++;
+// } while($i < 5);
+
+
+
+
+
+// for ( $i = 0; $i < 5; $i++ ){
+//     echo "hayu mabar <br>";
+// }
+
+
+?>
